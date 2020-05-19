dc142e855a42ff26ea8d22180d2681b2c34e2180
Volodymyr Protsenko <vovapro01@gmail.com>
2020-05-19 21:13:49 +0300

    fix codeStyle

--- src/main/java/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/cinema/dao/impl/MovieDaoImpl.java
@@ -5,24 +5,24 @@ import cinema.exceptions.DataProcessingException;
 import cinema.lib.Dao;
 import cinema.model.Movie;
 import cinema.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 import java.util.List;
 import javax.persistence.criteria.CriteriaQuery;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
         Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             Long itemId = (Long) session.save(movie);
             transaction.commit();
             movie.setId(itemId);
             return movie;
         } catch (Exception e) {
-            if(transaction != null) {
+            if (transaction != null) {
                 transaction.rollback();
             }
             throw new RuntimeException("Can't insert Movie Entity");
