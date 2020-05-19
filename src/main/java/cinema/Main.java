840f1b182f0a84f87e2a02a372fab254c24fccfb
Volodymyr Protsenko <vovapro01@gmail.com>
2020-05-19 18:59:34 +0300

    added movie model, dao, service and HibernateUtil added hibernate and mysql to pom.xml

--- src/main/java/cinema/Main.java
+++ src/main/java/cinema/Main.java
@@ -1,7 +1,18 @@
 package cinema;
 
-public class Main {
-    public static void main(String[] args) {
+import cinema.lib.Injector;
+import cinema.model.Movie;
+import cinema.service.MovieService;
 
+public class Main {
+    private static Injector injector = Injector.getInstance("cinema");
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Fast and Furious");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(movie);
+
+        movieService.getAll().forEach(System.out::println);
     }
 }
---
+++ src/main/java/cinema/service/impl/MovieSeviceImpl.java
@@ -0,0 +1,17 @@
+package cinema.service.impl;
+
+import cinema.model.Movie;
+import cinema.service.MovieService;
+import java.util.List;
+
+public class MovieSeviceImpl implements MovieService {
+    @Override
+    public Movie add(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        return null;
+    }
+}
