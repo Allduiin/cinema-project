ffbcc5611bfe3a900d49231a84553486d26f56a8
Volodymyr Protsenko <vovapro01@gmail.com>
2020-06-18 13:07:51 +0300

    Removed unnecessary import

--- src/main/java/cinema/model/User.java
+++ src/main/java/cinema/model/User.java
@@ -1,7 +1,6 @@
 package cinema.model;
 
 import java.util.Set;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
